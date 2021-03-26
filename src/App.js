fc601127e5dceb66abf2c3652582ae2f0c12b7cb
AbhishekBhattacharyya2020 <66897732+AbhishekBhattacharyya2020@users.noreply.github.com>
2021-03-26 16:14:25 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Test from './Test';
+import './App.css';
+
+
+const App =()=> {
+    return (
+      <div className="App">
+        <Test></Test>
+        <CssBaseline />
+      </div>
+    );
+}
+
+export default App;
